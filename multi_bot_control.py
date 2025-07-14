2364a5c08034c14b09d85bf1571a36f6b0957250
ndai98806 <ndai98806@gmail.com>
2025-07-14 20:41:53 +0700

    Update multi_bot_control.py

--- multi_bot_control.py
+++ multi_bot_control.py
@@ -29,7 +29,7 @@ karibbit_id = "1274445226064220273"
 # --- BIẾN TRẠNG THÁI ---
 bots, acc_names = [], [
     "Snoil", "Leaf", "Cheese", "Buterfly", "Ant", "Bee",
-    "Buterfly", "Ant", "Bee", "Bee", "White", "the Wicker", "Leader", "Tess", "Wyatt", "Daisy", "CantStop", "Token",
+    "001", "002", "003", "004", "White", "the Wicker", "Leader", "Tess", "Wyatt", "Daisy", "CantStop", "Token",
 ]
 main_bot, main_bot_2, main_bot_3 = None, None, None
 auto_grab_enabled, auto_grab_enabled_2, auto_grab_enabled_3 = False, False, False
